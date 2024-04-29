00c2b69d5bf81493fc3728cfa33d475f9f92d386
bhavinnirmal29 <bhavinnirmal29@gmail.com>
2024-04-28 23:06:24 -0600

    Microsoft Skills Added

--- src/app/components/skills/skills.component.ts
+++ src/app/components/skills/skills.component.ts
@@ -106,4 +106,36 @@ export class SkillsComponent {
       "icon": "devicon-postgresql-plain colored"
     }
   ];
+
+  tech_skills = [
+    { name: 'Microsoft Power Platform', icon: 'devicon-microsoft-plain' },
+    { name: 'Power Apps', icon: 'devicon-powerapps-plain' },
+    { name: 'Canvas Apps', icon: 'devicon-canvas-plain' },
+    { name: 'Dynamics 365 Plugins', icon: 'devicon-dynamics-plain' },
+    { name: 'Workflows', icon: 'devicon-workflow-plain' },
+    { name: 'Customization and Configuration', icon: 'devicon-customization-plain' },
+    { name: 'Power Automate Flows', icon: 'devicon-powerautomate-plain' },
+    { name: 'Microsoft Azure', icon: 'devicon-azure-plain' },
+    { name: 'Azure DevOps', icon: 'devicon-azuredevops-plain' },
+    { name: 'Azure Data Factory', icon: 'devicon-azuredatafactory-plain' },
+    { name: 'Azure Functions', icon: 'devicon-azurefunctions-plain' },
+    { name: 'Azure Logic Apps', icon: 'devicon-azurelogicapps-plain' },
+    { name: 'Azure Service Bus', icon: 'devicon-azureservicebus-plain' },
+    { name: 'CI/CD Pipelines', icon: 'devicon-cicd-plain' },
+    { name: 'SharePoint', icon: 'devicon-sharepoint-plain' },
+    { name: 'ServiceNow', icon: 'devicon-servicenow-plain' },
+    { name: 'Power BI', icon: 'devicon-powerbi-plain' },
+    { name: 'Microsoft 365', icon: 'devicon-microsoft365-plain' },
+    { name: 'Linux', icon: 'devicon-linux-plain' },
+    { name: 'Docker', icon: 'devicon-docker-plain' },
+    { name: 'Visual Studio', icon: 'devicon-visualstudio-plain' },
+    { name: 'NetBeans', icon: 'devicon-netbeans-plain' },
+    { name: 'Eclipse', icon: 'devicon-eclipse-plain' },
+    { name: 'VSCode', icon: 'devicon-vscode-plain' },
+    { name: 'Git', icon: 'devicon-git-plain' },
+    { name: 'GitHub', icon: 'devicon-github-plain' },
+    { name: 'GitHub Pages', icon: 'devicon-githubpages-plain' },
+    { name: 'GitHub Actions', icon: 'devicon-githubactions-plain' },
+    { name: 'Workflows', icon: 'devicon-workflow-plain' },
+  ];
 }
