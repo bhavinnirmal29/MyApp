0e03cc3fa02240b92a6c44a2147dbb0593dd95c7
bhavinnirmal29 <bhavinnirmal29@gmail.com>
2024-05-01 13:53:11 -0600

    Route TY Added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,6 +8,7 @@ import { ProjectsComponent } from './components/projects/projects.component';
 import { QualificationsComponent } from './components/qualifications/qualifications.component';
 import { SkillsComponent } from './components/skills/skills.component';
 import { HashLocationStrategy, LocationStrategy } from '@angular/common';
+import { ThankYouComponent } from './components/thank-you/thank-you.component';
 
 const routes: Routes = [
   {path:'',component:HomeComponent,pathMatch:'full'},
@@ -16,7 +17,8 @@ const routes: Routes = [
   {path:'WorkExperience',component:WorkExperienceComponent},
   {path:'Projects',component:ProjectsComponent},
   {path:'Qualifications',component:QualificationsComponent},
-  {path:'Skills',component:SkillsComponent}
+  {path:'Skills',component:SkillsComponent},
+  {path:'ThankYou',component:ThankYouComponent}
 ];
 
 @NgModule({
